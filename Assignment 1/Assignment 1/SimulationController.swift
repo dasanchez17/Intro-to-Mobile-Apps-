0b718a1c21b88e1115dea9abe3cf5a7b511ee185
dasanchez17 <danitravieso7@gmail.com>
2016-06-26 19:02:07 -0400

    Assignment 1

---
+++ Assignment 1/Assignment 1/SimulationController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Assignment 1
+//
+//  Created by Daniel Sanchez on 6/25/16.
+//  Copyright © 2016 Squad. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
